bc7129dbe455e5c3c1a2e61e206aafc03f1c3b7e
Martin Macharia <mosmartin5@gmail.com>
2020-10-15 08:23:02 +0200

    Create todos.ts
    Create the todos controller and add the create todos function.

---
+++ src/controllers/todos.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from "express";
+
+import { Todo } from "../models/todo";
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req.body as { text: string }).text;
+  const newToDo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newToDo);
+
+  res.status(201).json({
+    message: "todo created",
+    createdTodo: newToDo,
+  });
+};
