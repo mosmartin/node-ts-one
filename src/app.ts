0cfce84b65f2c1464aaa74c0703d1b9c085a36b3
Martin Macharia <mosmartin5@gmail.com>
2020-10-15 07:58:39 +0200

    Update app.ts
    Add the todos route handler to the server and handle errors.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+
+import todoRoutes from "./routes/todo";
 
 const app = express();
+
+// forward all /todos requests to the todos route handler
+app.use("/todos", todoRoutes);
+
+// error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    message: err.message,
+  });
+});
 
 app.listen(3000);
